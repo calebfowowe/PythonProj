da0a23b192993ba48830f3d411158dabb8798b40
Caleb Fowowe <calebfowowe@gmail.com>
2023-07-21 16:03:20 +0100

    Diamond solved

--- Textbook_exercise/Exercise2_5.py
+++ Textbook_exercise/Exercise2_5.py
@@ -1,4 +1,5 @@
-#Fibonacci Series
+"""
+#Exercise 9 Fibonacci Series
 fb_1 = 1
 fb_2 = 1
 num = int(input("Enter the number of fibonacci series you wish to print: "))
@@ -8,4 +9,37 @@ for i in range(2,num):
     print(fb_3, end=",")
     fb_1 = fb_2
     fb_2 = fb_3
+print()
+
+#Exercise 10
+height = eval(input('Enter the preferred height:'))
+width = eval(input('Enter the preferred width:'))
+for i in range (height):
+    print('*' * width)
     
+#Exercise 11
+height = eval(input('Enter the preferred height:'))
+width = eval(input('Enter the preferred width:'))
+first_row= '*' * width
+print(first_row)
+for i in range (2,height):
+    i = ('*') + (' '* (width-2)) + ('*')
+    print(i)
+print(first_row)
+
+#Exerice 12
+height = eval(input('Enter the height of the triangle:'))
+for i in range (height):
+    print('*' * (i+1))
+"""
+#Exercise13
+height = int(input('Enter the preferred height:'))
+h = int(height/2)
+for i in range (h):
+    w = h - i
+    z = (' '*(w)) + ('*'*((i+i)+1)) + (' '*(w)) 
+    print(z)
+for i in range (h,-1,-1):
+    w = h - i
+    z = (' '*(w)) + ('*'*((i+i)+1)) + (' '*(w)) 
+    print(z)
